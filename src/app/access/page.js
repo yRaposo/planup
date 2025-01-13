b27736d69236bf8be78c67e14e7a7056853b6662
yRaposo <ynyght@gmail.com>
2025-01-13 09:26:41 -0300

    Adiciona suporte a carregamento assíncrono na página de acesso com Suspense e refatora o componente Access

--- src/app/access/page.js
+++ src/app/access/page.js
@@ -2,15 +2,15 @@
 import AuthStatus from "@/components/AuthStatus";
 import { getAccessToken } from "@/service/authService";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, Suspense } from "react";
 import { AuthContext } from "@/context/AuthContext";
 import SuspenseWrapper from "@/components/SuspenseWrapper";
 
-export default function Access() {
+function AccessContent() {
   const searchParams = useSearchParams();
   const router = useRouter();
   const { token, setToken, refreshToken, setRefreshToken, isError, setIsError } = useContext(AuthContext);
-  const [code, setCode] = useState();
+  const [code, setCode] = useState('');
   const [state, setState] = useState('');
 
   useEffect(() => {
@@ -50,13 +50,21 @@ export default function Access() {
   }, [code, setToken, setRefreshToken, setIsError]);
 
   return (
+    <div className="flex flex-col gap-8 items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold">
+        Plan Up
+      </h1>
+      <AuthStatus isError={isError} token={token} code={code} />
+    </div>
+  );
+}
+
+export default function Access() {
+  return (
     <SuspenseWrapper>
-      <div className="flex flex-col gap-8 items-center justify-center min-h-screen">
-        <h1 className="text-4xl font-bold">
-          Plan Up
-        </h1>
-        <AuthStatus isError={isError} token={token} code={code} />
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <AccessContent />
+      </Suspense>
     </SuspenseWrapper>
   );
 }
