bec1a35f57b712c43a41067e790c91a0fbaaa1ce
yRaposo <ynyght@gmail.com>
2025-01-13 17:50:02 -0300

    altera a exportação da função Profile para exportação padrão e adiciona diretiva 'use client'

--- src/app/profiles/page.js
+++ src/app/profiles/page.js
@@ -1,9 +1,11 @@
-export function Profile() {
+'use client';
+
+export default function Profile() {
     return (
         <div className="m-10 flex justify-between align-middle">
             <div className="flex flex-col items-center aling-middle w-full">
                 <h1 className="justify-center items-center text-6xl font-bold text-center">PlanUp</h1>
             </div>
         </div>
-    )
+    );
 }
